c084699d5727aa3da435215d05bfdac38aabe8a6
moykul <moykul@live.com>
2022-06-23 17:07:11 +0100

    Update app.js
    Signed-off-by: moykul <moykul@live.com>

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import * as THREE from '../js/three.module.js';
-import { OrbitControls } from '../js/OrbitControls.js';
+import * as THREE from './js/three.module.js';
+import { OrbitControls } from './js/OrbitControls.js';
 
 class App{
 	constructor(){
